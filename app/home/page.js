930278a684bb855709b3c3ba0645e948c862d160
mrpanda1206 <karanpanda1206@gmail.com>
2025-01-29 19:06:27 +0530

    Merge conflict resolved

--- app/home/page.js
+++ app/home/page.js
@@ -1,11 +1,13 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
+import PublishModal from '../publish/page'
 
 const page = () => {
   return (
     <>
     {/* <Navbar/> */}
         <h1>Home</h1>
+        <PublishModal/>
     </>
   )
 }
